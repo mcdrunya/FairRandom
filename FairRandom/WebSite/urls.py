5007b4e32c79792e56f5a3c7ba38583019d72af2
Andrew <47192461+mcdrunya@users.noreply.github.com>
2022-07-29 23:12:44 +0300

    Delete FairRandom directory

--- FairRandom/WebSite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path('about', views.about, name="about"),
-    path('share/<str:url>', views.choose_time, name='choose_time'),
-]
-
