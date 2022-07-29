190d200c108e02014550ad250b917e9d2b3b773d
Andrew <47192461+mcdrunya@users.noreply.github.com>
2022-07-30 00:16:12 +0300

    Update views.py

--- WebSite/views.py
+++ WebSite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import ApplicationRandomModel
-from datetime import datetime
+from datetime import datetime, timezone
 import random
 import uuid
 
@@ -13,7 +13,7 @@ def index(request):
         max_range = request.POST.get('max')
         random_number = random.randint(int(min_range), int(max_range))
         _model = ApplicationRandomModel.objects.create(date_close=datetime.strptime(_datetime, '%Y-%m-%dT%H:%M'), 
-        date_open=datetime.now(), 
+        date_open=datetime.now(timezone.utc), 
         result=str(random_number),
         generated_amount=1, unique_url=uuid.uuid4().hex,
         _range= f"From {min_range} To {max_range}")
