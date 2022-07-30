9581e9d790d617a1d4aa050481e91b3cdc617464
Andrew <47192461+mcdrunya@users.noreply.github.com>
2022-07-30 18:34:20 +0300

    Update views.py

--- WebSite/views.py
+++ WebSite/views.py
@@ -8,19 +8,24 @@ def index(request):
     template_name = 'WebSite/index.html'
     
     if request.POST:
-        _datetime  = request.POST.get('appt')
-        min_range = request.POST.get('min')
-        max_range = request.POST.get('max')
-        client_time = request.POST.get('date-time')
-        random_number = random.randint(int(min_range), int(max_range))
-        _model = ApplicationRandomModel.objects.create(date_close=datetime.strptime(_datetime, '%Y-%m-%dT%H:%M'), 
-        date_open=datetime.strptime(client_time, '%Y-%m-%dT%H:%M:%S.%f%z'), 
-        result=str(random_number),
-        generated_amount=1, unique_url=uuid.uuid4().hex,
-        _range= f"From {min_range} To {max_range}")
+        try:
+            _datetime  = request.POST.get('appt')
+            min_range = request.POST.get('min')
+            max_range = request.POST.get('max')
+            client_time = request.POST.get('date-time')
+            random_number = random.randint(int(min_range), int(max_range))
+            _model = ApplicationRandomModel.objects.create(date_close=datetime.strptime(_datetime, '%Y-%m-%dT%H:%M'), 
+            date_open=datetime.strptime(client_time, '%Y-%m-%dT%H:%M:%S.%f%z'), 
+            result=str(random_number),
+            generated_amount=1, unique_url=uuid.uuid4().hex,
+            _range= f"From {min_range} To {max_range}")
+            
+            return redirect(f'share/{_model.unique_url}')
         
-        return redirect(f'share/{_model.unique_url}')
-
+        except Exception as e:
+            print(e)
+            return redirect('/')
+    
     return render(request, template_name)
 
 
